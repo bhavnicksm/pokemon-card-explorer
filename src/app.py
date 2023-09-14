aa32556c19917d4f7d089b083081cd57f55459b9
bhavnicksm <bhavnicksm@gmail.com>
2023-09-14 10:12:24 +0530

    Add API keys to the environment

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
+import os
+
 import streamlit as st
 import pinecone
 import cohere 
 
-from keys import COHERE_API_KEY,PINECONE_API_KEY
- 
+COHERE_API_KEY = os.environ['COHERE_API_KEY']
+PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
+
 def init_pinecone():
     # find API key at app.pinecone.io
     pinecone.init(api_key=PINECONE_API_KEY, environment="us-west1-gcp")
