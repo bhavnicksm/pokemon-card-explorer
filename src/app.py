3175821307a3ae507fdcf48d5cad2a86169c6519
bhavnicksm <bhavnicksm@gmail.com>
2024-01-16 20:54:34 +0530

    Update app.py for qdrant search

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 import os
 
 import streamlit as st
-import pinecone
 import cohere 
 import openai
+
+from qdrant_client import QdrantClient
+
 
 st.set_page_config(
     page_title="Pokemon Card Explorer",
@@ -18,18 +20,26 @@ st.set_page_config(
 
 OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
 COHERE_API_KEY = os.environ['COHERE_API_KEY']
-PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
+QDRANT_API_KEY = os.environ['QDRANT_API_KEY']
 
-def init_pinecone():
-    # find API key at app.pinecone.io
-    pinecone.init(api_key=PINECONE_API_KEY, environment="us-west1-gcp")
-    return pinecone.Index('pokemon-cards-v2')
+# def init_pinecone():
+#     # find API key at app.pinecone.io
+#     pinecone.init(api_key=PINECONE_API_KEY, environment="us-west1-gcp")
+#     return pinecone.Index('pokemon-cards-v2')
+
+def init_qdrant():
+    # initialise the qdrant_client
+    qdrant_client = QdrantClient(
+        url = "https://b8f17857-c547-4c58-8ba8-b479a15dacad.us-east4-0.gcp.cloud.qdrant.io:6333", 
+        api_key=QDRANT_API_KEY,
+    )
+    return qdrant_client
     
 def init_reranker():
     return cohere.Client(COHERE_API_KEY) 
 
 
-index = init_pinecone()
+index = init_qdrant()
 reranker = init_reranker()
 retriever = openai.Embedding
 
@@ -47,7 +57,6 @@ def card(urls):
 
 
 st.image('assets/logo.png')
-# st.markdown("<h1 style='text-align: center;'>Pokemon Card Explorer</h1>", unsafe_allow_html=True)
 query = st.text_input(label="**Search:**", placeholder="Describe the Pokemon Card you are looking for...", )
 
 if query != "":
@@ -56,16 +65,17 @@ if query != "":
         resp = retriever.create(model="text-embedding-ada-002", input=query)
         qemb = resp['data'][0]['embedding']
 
-        # Use the pinecone index to get top 6 results
-        qcs = index.query(qemb, top_k=6, include_metadata=True)
+        # Use the qdrant index to get top 6 results
+        # qcs = index.query(qemb, top_k=6, include_metadata=True)
+        qcs = index.search('pokemon-cards', query_vector=qemb, limit=6, with_payload=True)
 
-        docs = [qc['metadata']['description'] for qc in qcs['matches']]
-        rr_resp = reranker.rerank(model="rerank-english-v2.0", query=query, documents=docs, top_n=6)
-        reranked_index = [rr_resp[i].index for i in range(len(docs))]
+        qtexts = [sp.payload['text'] for sp in qcs]
+        rr_resp = reranker.rerank(model="rerank-english-v2.0", query=query, documents=qtexts, top_n=6)
+        reranked_index = [rr_resp[i].index for i in range(6)]
 
-        rr_qcs = [qcs['matches'][ind] for ind in reranked_index]
+        rr_qcs = [qcs[ind].payload for ind in reranked_index]
         
-        urls = [x['metadata']['img_url'] for x in rr_qcs]
+        urls = [x['card_image'] for x in rr_qcs]
 
     with st.spinner(text="Loading Cards"):
         card(urls)
